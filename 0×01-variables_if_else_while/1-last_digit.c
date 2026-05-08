8e43c639dcf3dd2294eb05b6e90df988345e0102
SAM <agbaisamuel13@gmail.com>
2026-05-08 17:16:39 +0100

    Add program that prints the last digit of a random number with custom messages

---
+++ 0×01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+/**
+ * Author: Agbai Samuel Chibuike
+ * Program: WinMingle Community C Training
+ * Description: This is a program that prints the last digit of a random number n.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int n;
+	int x;
+	srand(time(0));
+	n=rand() - RAND_MAX/2;
+	x=n%10;
+		if(x>5)
+			printf("Last digit of %d is %d and is greater than 5\n", n, x);
+		else if(x==0)
+			printf("Last digit of %d is %d and is o 0\n", n, x);
+		else
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+	return(0);
+}
