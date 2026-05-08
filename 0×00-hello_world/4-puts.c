c1e54936c8749a20aca5cab982bc3c087c5a79ac
SAM <agbaisamuel13@gmail.com>
2026-05-08 16:22:28 +0100

    Add program that prints using puts

---
+++ 0×00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ * Author: Agbai Samuel Chibuike
+ * Program: WinMingle Community C Training
+ * Description: This is a program that prints a given line, followed by a new line using 'puts' function.
+ */
+#include<stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
