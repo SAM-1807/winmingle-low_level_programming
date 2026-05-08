237e3596f32ba8c0e83ffd2619c07f260a488248
SAM <agbaisamuel13@gmail.com>
2026-05-08 16:22:28 +0100

    Add program that prints using printf

---
+++ 0×00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * Author: Agbai Samuel Chibuike
+ * Program: WinMingle Community C Training
+ * Description: This is a program that prints a given line, followed by a new line using 'printf' function.
+ */
+#include<stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
