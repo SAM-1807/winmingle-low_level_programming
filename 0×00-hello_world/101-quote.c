1b56e847ee836ff921e5535127c04241d9d42876
SAM <agbaisamuel13@gmail.com>
2026-05-08 16:22:28 +0100

    Add program that prints quote to stderr

---
+++ 0×00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * Author: Agbai Samuel Chibuike
+ * Program: WinMingle Community C Training
+ * Description: This is a program that prints a given quote to the standard error.
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
