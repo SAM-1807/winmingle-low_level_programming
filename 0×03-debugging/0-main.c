b36f263742127a725abd9d5f1f4ee88a3c28e674
SAM <agbaisamuel13@gmail.com>
2026-05-08 18:29:16 +0100

    Add test file for positive_or_negative function with 0

---
+++ 0×03-debugging/0-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * This is a file that tests a function, to indicate if a number is positive, negative or zero, using 0 as input.
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+	return(0);
+}
