4d589ec82b029dec5c059ceb8461117f44e9ceda
SAM <agbaisamuel13@gmail.com>
2026-07-13 18:35:06 +0100

    Add strncpy: copies at most n bytes from src

---
+++ 0×06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+	while(n > 0 && *src != '\0')    {
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
+	while(n > 0)    {
+		*ptr = '\0';
+		ptr++;
+		n--;
+	}
+	return dest;
+}
