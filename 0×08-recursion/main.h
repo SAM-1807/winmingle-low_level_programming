f2955911cc95a68664d77e1a3e6aa93a35d34c11
SAM <agbaisamuel13@gmail.com>
2026-09-11 00:27:44 +0100

    Pushing my recursion tasks 5 and 6 from my phone.

---
+++ 0×08-recursion/main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Task 0 */
+void _puts_recursion(char *s);
+
+/* Task 1 */
+void _print_rev_recursion(char *s);
+
+/* Task 2 */
+int _strlen_recursion(char *s);
+
+/* Task 3 */
+int factorial(int n);
+
+/* Task 4 */
+int _pow_recursion(int x, int y);
+
+/* Task 5 */
+int _sqrt_recursion(int n);
+
+/* Task 6 */
+int is_prime_number(int n);
+
+/* Task 7 */
+int is_palindrome(char *s);
+
+/* Task 8 */
+int wildcmp(char *s1, char *s2);
+
+/* _putchar (WinMingle provides this) */
+int _putchar(char c);
+
+#endif
