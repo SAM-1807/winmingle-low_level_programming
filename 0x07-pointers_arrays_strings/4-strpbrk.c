e859af839d7986890ddc811614cafb3fa1101194
SAM <agbaisamuel13@gmail.com>
2026-09-16 18:04:59 +0100

    Fix empty 0x07 files: add strpbrk, strstr, print_chessboard, print_diagsums, set_string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - finds the first occurrence in s of any byte from accept.
+ * @s: the string to search.
+ * @accept: the set of bytes to look for.
+ *
+ * Return: a pointer to the first matching byte, or NULL if none found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring needle inside haystack.
+ * @haystack: the string to search.
+ * @needle: the substring to look for.
+ *
+ * Return: a pointer to the start of the match, or NULL if not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (*needle == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+
+	return (NULL);
+}
